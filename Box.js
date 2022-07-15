d2cf2a9edcee6bed2ecec4113e905acd5a6f397b
Andrés Manikis <andresmanikis@gmail.com>
2022-07-16 00:47:36 +0100

    Creates Box class

--- Box.js
+++ Box.js
@@ -4,5 +4,14 @@ export class Box {
     this.x = x;
     this.y = y;
     this.moving = false;
+    this.update();
+
+    this.el.addEventListener("mousedown", () => (this.moving = true));
+    this.el.addEventListener("mouseup", () => (this.moving = false));
+  }
+
+  update() {
+    this.el.style.left = this.x;
+    this.el.style.top = this.y;
   }
 }
--- main.js
+++ main.js
@@ -1,38 +1,26 @@
-const box = document.querySelector("div");
+import { Box } from "./Box.js";
 
-let moving = false;
-
-let boxX = 200;
-let boxY = 100;
+const box = new Box(document.querySelector("div"), 200, 100);
 
 let prevMouseX = 0;
 let prevMouseY = 0;
-
-function updateBox() {
-  box.style.left = boxX;
-  box.style.top = boxY;
-}
-
-updateBox();
 
 document.addEventListener("mousedown", (e) => {
   prevMouseX = e.clientX;
   prevMouseY = e.clientY;
 });
-box.addEventListener("mousedown", () => (moving = true));
-box.addEventListener("mouseup", () => (moving = false));
 
 document.addEventListener("mousemove", (e) => {
-  if (!moving) return;
-
   const deltaX = e.clientX - prevMouseX;
   const deltaY = e.clientY - prevMouseY;
 
-  boxX += deltaX;
-  boxY += deltaY;
-
-  updateBox();
-
   prevMouseX = e.clientX;
   prevMouseY = e.clientY;
+
+  if (box.moving) {
+    box.x += deltaX;
+    box.y += deltaY;
+
+    box.update();
+  }
 });
