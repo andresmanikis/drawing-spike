fbdf015fed83b1bd1af3bf450a5a768a611f23aa
Andrés Manikis <andresmanikis@gmail.com>
2022-07-16 01:29:16 +0100

    Adds a second box

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 import { Box } from "./Box.js";
 import { onMouseDelta } from "./onMouseDelta.js";
 
-const box = new Box(200, 100);
+const box1 = new Box(200, 100);
+const box2 = new Box(400, 200);
 
 onMouseDelta((deltaX, deltaY) => {
-  box.handleMouseDelta(deltaX, deltaY);
+  box1.handleMouseDelta(deltaX, deltaY);
+  box2.handleMouseDelta(deltaX, deltaY);
 });
 
 document.addEventListener("mouseup", () => {
-  box.stopInteractions();
+  box1.stopInteractions();
+  box2.stopInteractions();
 });
