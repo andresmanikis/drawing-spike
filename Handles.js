38fe5403e67b0597387a8833332996ed40e89787
Andrés Manikis <andresmanikis@gmail.com>
2022-09-28 21:14:06 +0100

    Extracts handles css

--- Handles.js
+++ Handles.js
@@ -1,11 +1,10 @@
-const offset = 8;
+const offset = 0;
 
 export class Handles {
   constructor(x, y, width, height) {
     const el = document.createElement("div");
+    el.className = "handles";
 
-    el.style.border = "2px solid green";
-    el.style.position = "fixed";
     el.style.left = x - offset / 2 - width / 2;
     el.style.top = y - offset / 2 - height / 2;
     el.style.width = width + offset;
