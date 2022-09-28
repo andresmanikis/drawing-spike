d8d31ef3ecf554aa670bebc4f5d6bd7465f5f333
Andrés Manikis <andresmanikis@gmail.com>
2022-09-28 23:33:48 +0100

    Allows placing handles on any box

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ import { Handles } from "./Handles.js";
 
 const boxes = [new Box(200, 100, "blue"), new Box(400, 200, "green")];
 
-new Handles(boxes[0]);
+let handles = null;
+
+boxes.forEach((box) => {
+  box.onMouseDown(() => {
+    handles?.remove();
+    handles = new Handles(box);
+  });
+});
+
+document.addEventListener("mousedown", (e) => {
+  if (e.target === document.body) {
+    handles?.remove();
+    handles = null;
+  }
+});
